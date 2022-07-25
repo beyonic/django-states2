ae051335513b40b7cca5935ab0a6470f38320e61
Luke Kyohere <KayLuke@users.noreply.github.com>
2022-07-25 06:12:49 -0400

    Django 4 updates

--- django_states/log.py
+++ django_states/log.py
@@ -7,8 +7,8 @@ import sys
 
 from django.db import models
 from django.db.models.base import ModelBase
-from django.utils.encoding import python_2_unicode_compatible
-from django.utils.translation import ugettext_lazy as _
+from six import python_2_unicode_compatible
+from django.utils.translation import gettext_lazy as _
 from django.conf import settings
 
 from django_states import conf
